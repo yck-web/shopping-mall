b96fdf66b141f3e9703674e7a464c4b30c7f6242
yck-web <1183249118@qq.com>
2022-01-28 00:24:45 +0800

    谷粒商城初创建v1.0

---
+++ gulimali-ware/src/main/java/com/atguigu/gulimall/gulimaliware/GulimaliWareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimaliware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimaliWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimaliWareApplication.class, args);
+    }
+
+}
---
+++ gulimall-number/src/test/java/com/atguigu/gulimall/gulimallnumber/GulimallNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimallnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
